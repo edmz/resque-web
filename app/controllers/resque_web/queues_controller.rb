ee1e56763f1d6c5ba669a5aff8c915c570b0200d
edmz <edmz@users.noreply.github.com>
2024-01-12 18:00:01 -0600

    queues view: adding json format

--- app/controllers/resque_web/queues_controller.rb
+++ app/controllers/resque_web/queues_controller.rb
@@ -2,6 +2,14 @@ module ResqueWeb
   class QueuesController < ResqueWeb::ApplicationController
 
     def index
+      respond_to do |format|
+        format.html
+        format.json do
+          Resque.queues.sort_by(&:to_s).each_with_object({}) do |queue_name, memo|
+            memo[queue_name] = Redis.queue_size(queue_name)
+          end
+        end
+      end
     end
 
     def show
